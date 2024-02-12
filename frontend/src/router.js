4e00c78ba8b3c82ad27a6d5734af3c69ad29d0f2
GennW <778855@mail.ru>
2024-02-12 13:41:08 +0300

    работает авторизация с запросом токенов, при переходе на главную страницу не созраняются данные

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -63,6 +63,12 @@ export class Router {
                 },
                 styles: ['icheck-bootstrap.min.css'],
             },
+            {
+                route: '/logout',
+                load: () => {
+
+                }
+            },
         ]
     }
 
